1ea3e94065f17425cd71f389a9079840dc1f6ee1
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-03-04 22:05:10 +0200

    Responsive design was finished

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,25 +11,20 @@ export const Navbar = () => {
 	};
 
 	return (
-		<div className='gap-5 mb-5 flex justify-between items-center p-4'>
+		<div className='gap-5 mb-5 flex justify-between items-center p-2 sm:p-4'>
 			{/* Left side */}
 			<div className='flex items-center gap-3'>
 				<div onClick={menuHandle} className='cursor-pointer'>
 					<AiOutlineMenu size={30} />
 				</div>
 
-				<h1 className='text-2xl sm:text-3xl lg:text-4xl'>
+				<h1 className='hidden sm:block text-2xl sm:text-3xl lg:text-4xl'>
 					Best <span className='font-bold'>Eats</span>
 				</h1>
-
-				<div className='hidden items-center gap-2 bg-gray-200 rounded-full p-1 text-sm lg:flex'>
-					<p className='bg-black text-white rounded-full p-2'>Delivery</p>
-					<p>Pickup</p>
-				</div>
 			</div>
 
 			{/* Search */}
-			<div className='flex items-center gap-2 bg-gray-200 rounded-full p-2 w-54 sm:w-[500px]'>
+			<div className='flex items-center gap-2 bg-gray-200 rounded-full p-2  lg:w-[500px]'>
 				<AiOutlineSearch size={20} />
 				<input
 					type='text'
