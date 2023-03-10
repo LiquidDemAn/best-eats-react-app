2924f7ecabc722ce8823ceb983efb077e2667e73
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-03-10 21:27:25 +0200

    Plus button in FoodItem component was created

--- src/components/FoodItem/FodItem.tsx
+++ src/components/FoodItem/FodItem.tsx
@@ -1,4 +1,5 @@
 import { MenuItemType } from '../../typedef';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 type Props = {
 	item: MenuItemType;
@@ -8,12 +9,15 @@ export const FoodItem = ({ item }: Props) => {
 	const { name, price, image } = item;
 
 	return (
-		<div className='border w-full shadow-lg hover:scale-105 transition rounded-lg cursor-pointer'>
-			<img
-				className='w-full h-52 object-cover rounded-t-lg '
-				src={image}
-				alt={name}
-			/>
+		<div className='relative border w-full shadow-lg hover:scale-105 rounded-lg cursor-pointer group transition'>
+			<div
+				style={{ backgroundImage: `url(${image})` }}
+				className='flex justify-center items-center h-52 bg-center bg-cover'
+			>
+				<button className='opacity-0 group-hover:opacity-100 rounded-full p-4 bg-orange-500 text-white border-transparent transition'>
+					<AiOutlinePlus size={30} />
+				</button>
+			</div>
 			<div className='flex items-center justify-between px-2 py-4 gap-1'>
 				<p className='font-bold'>{name}</p>
 				<p>
